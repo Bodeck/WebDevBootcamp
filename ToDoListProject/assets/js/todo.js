1c4de2e02a45cf042c26d1c076ca3f592ee9c07b
Bodzio Franczak <bfranczak@gmail.com>
2018-06-03 14:14:23 +0200

    replaced click with 'on' event so new items behaviour

--- ToDoListProject/assets/js/todo.js
+++ ToDoListProject/assets/js/todo.js
@@ -1,14 +1,22 @@
 document.addEventListener("DOMContentLoaded", main, false);
 function main() {
-    $('input').keypress(function (event) {
+    $('input[type="text"]').keypress(function (event) {
         if (event.which === 13) {
             var itemTxt = $(this).val();
             $(this).val("");
-            $("#list").append('<li class="li"><span class="trashicon">X</span>' + itemTxt + '</li>');
+            $("#list").append('<li class="li"><span class="trashicon">X</span> ' + itemTxt + '</li>');
         }
     })
 
-    $('li').click(function(){
+    $('ul').on('click', 'li', function () {
         $(this).toggleClass("completed");
     })
+
+    $('ul').on('click', 'span', function (event) {
+        $(this).parent().fadeOut(function () {
+            $(this).remove();
+        });
+        event.stopPropagation();
+    })
+
 };
